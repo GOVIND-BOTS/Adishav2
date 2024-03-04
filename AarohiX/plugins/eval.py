ce220680e9055af6ca66549c45a6f4bb19e4195c
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-03-04 17:21:02 +0530

    Update eval.py

--- AarohiX/plugins/eval.py
+++ AarohiX/plugins/eval.py
@@ -5,7 +5,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import OWNER_ID
-from AarohiX import app as bot
+from AarohiX import app 
 
 
 
